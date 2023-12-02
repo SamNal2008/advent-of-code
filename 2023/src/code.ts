6c85cbb122f7b6cea57b15519f4ca1c5fe008f33
samy <samy.nalbandian@epita.fr>
2023-12-02 15:20:17 +0100

    feat: day 1

---
+++ 2023/src/code.ts
@@ -0,0 +1,36 @@
+
+export const isNumber = (char: string) => {
+  return !isNaN(parseInt(char));
+};
+
+export const analyzeOneLine = (line: string) => {
+  let leftNumberValue = null;
+  let rightNumberValue = null;
+  for (let i = 0; i < line.length; i++) {
+    const leftNumber = line[ i ];
+    const rightNumber = line[ line.length - i - 1 ];
+    if (isNumber(leftNumber) && leftNumberValue === null) {
+      leftNumberValue = parseInt(leftNumber);
+    }
+    if (isNumber(rightNumber) && rightNumberValue === null) {
+      rightNumberValue = parseInt(rightNumber);
+    }
+  }
+  if (leftNumberValue === null) {
+    leftNumberValue = 0;
+  }
+  if (rightNumberValue === null) {
+    rightNumberValue = leftNumberValue;
+  }
+  return ( leftNumberValue * 10 ) + rightNumberValue;
+};
+
+export const analyze = (input: string) => {
+  const lines = input.split('\n');
+  let sum = 0;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[ i ];
+    sum += analyzeOneLine(line);
+  }
+  return sum;
+}
---
+++ 2023/src/day1.test.ts
@@ -0,0 +1,49 @@
+import { readFileSync } from 'fs';
+import { analyze, analyzeOneLine } from './code';
+
+
+describe('day1', () => {
+  describe('Row extraction', () => {
+    it('return the concat of the only number present', () => {
+      const input = '1';
+      expect(analyzeOneLine(input)).toBe(11);
+    });
+
+    it('return the concat of the only number present', () => {
+      const input = '2';
+      expect(analyzeOneLine(input)).toBe(22);
+    });
+
+    it('return the concat of the only number present', () => {
+      const input = '23';
+      expect(analyzeOneLine(input)).toBe(23);
+    });
+
+    it('should return the concat of the first numbers', () => {
+      const input = '123';
+      expect(analyzeOneLine(input)).toBe(13);
+    });
+
+    it('should return 0 when no number is present', () => {
+      const input = 'abc';
+      expect(analyzeOneLine(input)).toBe(0);
+    });
+
+    it('should return 0 when no number is present', () => {
+      const input = 'abc123';
+      expect(analyzeOneLine(input)).toBe(13);
+    });
+  });
+
+  describe('Sum of all rows', () => {
+    it('should return the sum of all rows', () => {
+      const input = '1\n2\n3';
+      expect(analyze(input)).toBe(66);
+    });
+
+    it('should respect the example', () => {
+      const input = readFileSync('./src/input.txt', 'utf8');
+      expect(analyze(input)).toBe(55172);
+    });
+  });
+});
