383f37c3d3a09491fcc797a5776fb6817498e891
samy <samy.nalbandian@epita.fr>
2023-12-02 15:48:38 +0100

    feat: add extract number method to utils

--- 2023/src/utils/numbers.utils.test.ts
+++ 2023/src/utils/numbers.utils.test.ts
@@ -1,13 +1,30 @@
-import { isNumberUnder10 } from './numbers.utils';
+import { extractNumber, isNumberUnder10 } from './numbers.utils';
 
 describe('Numbers utils', () => {
-  it('should return true when the value is a number', () => {
-    expect(isNumberUnder10('1')).toBeTruthy();
+
+  const INVALID_NUMBERS_UNDER_10 = [null, undefined, 'a', '1a', '10'];
+
+  describe('isNumberUnder10', () => {
+    it('should return true when the value is a number', () => {
+      expect(isNumberUnder10('1')).toBeTruthy();
+    });
+
+    it.each
+    (INVALID_NUMBERS_UNDER_10)
+    (`should return false when the value is not a number : %s`, (value) => {
+      expect(isNumberUnder10(value)).toBeFalsy();
+    });
   });
 
-  it.each
-  ([null, undefined, 'a', '1a', '10'])
-  (`should return false when the value is not a number : %s`, (value) => {
-    expect(isNumberUnder10(value)).toBeFalsy();
+  describe('extractNumber', () => {
+    it('should return the number when the value is a number', () => {
+      expect(extractNumber('1')).toBe(1);
+    });
+
+    it.each
+    (INVALID_NUMBERS_UNDER_10)
+    ('should throw an error when the value is not a number : %s', (invalidNumber) => {
+      expect(() => extractNumber(invalidNumber)).toThrow('The char is not a number under 10');
+    });
   });
 });
--- 2023/src/utils/numbers.utils.ts
+++ 2023/src/utils/numbers.utils.ts
@@ -4,3 +4,11 @@ export const isNumberUnder10 = (char?: string | null) => {
   }
   return !isNaN(parseInt(char));
 }
+
+export const extractNumber = (char?: string | null) => {
+  if (!isNumberUnder10(char)) {
+    throw new Error('The char is not a number under 10');
+  }
+  const numberUnder10AsString = char as string;
+  return parseInt(numberUnder10AsString);
+}
