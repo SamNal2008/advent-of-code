225b4e28faad81e16099a1f03e5ff4cfd2beab8a
samy <samy.nalbandian@epita.fr>
2023-12-03 13:02:26 +0100

    refacto: extract method to extract number if present

--- 2023/src/code.ts
+++ 2023/src/code.ts
@@ -1,18 +1,9 @@
 import { extractNumber, isNumberUnder10 } from './utils/numbers.utils';
 
-function extractLeftNumberIfPresent(currentChar: string, leftNumberValue?: number): number | undefined {
-  if (isNumberUnder10(currentChar) && leftNumberValue === undefined) {
-    leftNumberValue = extractNumber(currentChar);
-  }
-  return leftNumberValue;
-}
-
-const extractRightNumberIfPresent = (currentChar: string, rightNumberValue?: number): number | undefined => {
-  if (isNumberUnder10(currentChar)) {
-    rightNumberValue = extractNumber(currentChar);
-  }
-  return rightNumberValue;
-}
+const extractNumberIfPresent = (currentChar: string, shouldBeReplaced: boolean, initialValue?: number): number | undefined => {
+  const shouldExtractNumber = isNumberUnder10(currentChar) && shouldBeReplaced;
+  return shouldExtractNumber ? extractNumber(currentChar) : initialValue;
+};
 
 const computeLineResult = (leftNumberValue?: number, rightNumberValue?: number): number => {
   if (leftNumberValue === undefined) {
@@ -22,15 +13,15 @@ const computeLineResult = (leftNumberValue?: number, rightNumberValue?: number):
     rightNumberValue = leftNumberValue;
   }
   return ( leftNumberValue * 10 ) + rightNumberValue;
-}
+};
 
 export const analyzeOneLine = (line: string): number => {
   let leftNumberValue = undefined;
   let rightNumberValue = undefined;
   for (let i = 0; i < line.length; i++) {
     const currentChar = line[ i ];
-    leftNumberValue = extractLeftNumberIfPresent(currentChar, leftNumberValue);
-    rightNumberValue = extractRightNumberIfPresent(currentChar, rightNumberValue);
+    leftNumberValue = extractNumberIfPresent(currentChar, leftNumberValue === undefined, leftNumberValue);
+    rightNumberValue = extractNumberIfPresent(currentChar, true, rightNumberValue);
   }
   return computeLineResult(leftNumberValue, rightNumberValue);
 };
